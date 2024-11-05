1ef636b153c96e7afd881498a4b4bc7cba656704
Turchiellows <[turchiellows@yahoo.com.br](mailto:turchiellows@yahoo.com.br)>
2024-11-05 20:13:10 -0200

    init: Início do projeto

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,19 @@
+// PACOTES
+package principal;
+
+import visao.Index;
+
+public class Principal {
+   public static void main(String[] args) {
+      Index index = new Index();
+      
+      index.setVisible(true);
+      index.setLocationRelativeTo(null);
+      
+
+      
+      
+      
+      
+   }
+}
